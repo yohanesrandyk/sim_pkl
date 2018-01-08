e5fafa096d82459ebcb49f81f745eb527893a503
Dicki Fadilah Fajar <fadilahfajar74@gmail.com>
2018-01-08 12:43:46 +0700

    Dicki Fadilah Fajar
    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,8 +51,8 @@
                 <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
 
                 <a href="{{ route('password.request') }}"><small>Forgot password?</small></a>
-                <p class="text-muted text-center"><small>Do not have an account?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
+                {{-- <p class="text-muted text-center"><small>Do not have an account?</small></p>
+                <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a> --}}
             </form>
             <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
         </div>
